b3bfb3f8708b124b5cec453cc931908738a1c9b6
zhoujun <849408087@qq.com>
2019-05-11 09:58:03 +0800

    注释中文化， 该步骤主要是两步： 1. 自动安装包。 2. 文件扩展名自动关联模式。

--- core/prelude-packages-1.el
+++ core/prelude-packages-1.el
@@ -1,19 +1,19 @@
-; emacs中的package包管理插件
-; 该文件主要关注如何自动安装当前emacs正常运行所需要的安装包
+;; emacs中的package包管理插件
+;; 该文件主要关注如何自动安装当前emacs正常运行所需要的安装包
 (require 'cl)
 (require 'package)
 
-; 添加package-archives
+;; 添加package-archives
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 			 
-; 定义package-user-dir目录
+;; 定义package-user-dir目录
 (setq package-user-dir (expand-file-name "elpa" prelude-dir))
 
-; 包管理插件初始化
+;; 包管理插件初始化
 (package-initialize)
 
-; 定义prelude-packages变量
+;; 定义prelude-packages变量
 (defvar prelude-packages
   '(ace-window
     avy
@@ -49,11 +49,12 @@
     zop-to-char)
   "A list of packages to ensure are installed at launch.")
 
-
+;; 检查是否数组prelude-packages中的包全部安装
 (defun prelude-packages-installed-p ()
   "Check if all packages in `prelude-packages' are installed."
   (every #'package-installed-p prelude-packages))
 
+;; 如果传入的包名没有安装，先
 (defun prelude-require-package (package)
   "Install PACKAGE unless already installed."
   (unless (memq package prelude-packages)
@@ -61,17 +62,16 @@
   (unless (package-installed-p package)
     (package-install package)))
 
+;; 检查传入数组的包，如果没有安装，则安装。
 (defun prelude-require-packages (packages)
   "Ensure PACKAGES are installed.
 Missing packages are installed automatically."
   (mapc #'prelude-require-package packages))
 
-  
-
+;; 定义别名
 (define-obsolete-function-alias 'prelude-ensure-module-deps 'prelude-require-packages)
 
-
-
+;; 安装包入口函数
 (defun prelude-install-packages ()
   "Install all packages listed in `prelude-packages'."
   (unless (prelude-packages-installed-p)
@@ -82,12 +82,12 @@ Missing packages are installed automatically."
     ;; install the missing packages
     (prelude-require-packages prelude-packages)))
 
-;; run package installation
+;; 调用安装包
 (prelude-install-packages)
 
 
-
-
+;---------------------------------------------------------------------------------- 
+;; 列出已安装的第三方但不在prelude-packages数组里面的包，方便卸载
 (defun prelude-list-foreign-packages ()
   "Browse third-party packages not bundled with Prelude.
 
@@ -98,6 +98,7 @@ removing unwanted packages."
   (package-show-package-list
    (set-difference package-activated-list prelude-packages)))
 
+;; 文件扩展名和对应的mode加入到auto-mode-alist关联
 (defmacro prelude-auto-install (extension package mode)
   "When file with EXTENSION is opened triggers auto-install of PACKAGE.
 PACKAGE is installed only if not already present.  The file is opened in MODE."
@@ -107,6 +108,7 @@ PACKAGE is installed only if not already present.  The file is opened in MODE."
                                    (package-install ',package))
                                  (,mode)))))
 
+;; 模式数组定义
 (defvar prelude-auto-install-alist
   '(("\\.clj\\'" clojure-mode clojure-mode)
     ("\\.cmake\\'" cmake-mode cmake-mode)
@@ -154,8 +156,8 @@ PACKAGE is installed only if not already present.  The file is opened in MODE."
     ("\\.yaml\\'" yaml-mode yaml-mode)
     ("Dockerfile\\'" dockerfile-mode dockerfile-mode)))
 
-;; markdown-mode doesn't have autoloads for the auto-mode-alist
-;; so we add them manually if it's already installed
+;; markdown模式无法自动加载到auto-mode-alist，
+;; 所以如果安装了，我们手动进行加载。
 (when (package-installed-p 'markdown-mode)
   (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
   (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode)))
@@ -164,7 +166,7 @@ PACKAGE is installed only if not already present.  The file is opened in MODE."
   (add-to-list 'auto-mode-alist '("PKGBUILD\\'" . pkgbuild-mode)))
 
 
-;; build auto-install mappings
+;; 模式的自动映射
 (mapc
  (lambda (entry)
    (let ((extension (car entry))
@@ -174,9 +176,6 @@ PACKAGE is installed only if not already present.  The file is opened in MODE."
        (prelude-auto-install extension package mode))))
  prelude-auto-install-alist)
 
+ ;; 导出模块
 (provide 'prelude-packages-1)
-;; Local Variables:
-;; byte-compile-warnings: (not cl-functions)
-;; End:
 
-;;; prelude-packages.el ends here
